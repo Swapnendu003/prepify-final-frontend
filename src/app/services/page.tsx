1db8669073b06e1c934784e04da77bdf9c7ac79b
Swapnendu003 <126004998+Swapnendu003@users.noreply.github.com>
2024-06-29 16:58:35 +0530

    added the service page

---
+++ src/app/services/page.tsx
@@ -0,0 +1,17 @@
+import Footer from '@/components/Common/Footer/Footer';
+import Navbar from '@/components/Common/Navbar';
+import {  CardHoverEffectDemo } from '@/components/Services/CardHoverEffectDemo'
+//import Service from '@/components/Services/Service';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div style={{height:'100vh'}}>
+      < CardHoverEffectDemo />
+       
+
+    </div>
+  );
+}
+
+export default page;
